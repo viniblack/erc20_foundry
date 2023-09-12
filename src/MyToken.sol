d76c452d6b11d6694a7afc1465be151b8db2b7d4
Vini Black <51183682+viniblack@users.noreply.github.com>
2023-09-12 19:23:35 -0300

    Update MyToken.sol

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin/token/ERC20/ERC20.sol";
+import "openzeppelin/token/ERC20/extensions/ERC20Burnable.sol";
+import "openzeppelin/access/Ownable.sol";
 
 contract MyToken is ERC20, ERC20Burnable, Ownable {
     constructor (string memory _name, string memory _symbol) ERC20 (_name,_symbol) {
